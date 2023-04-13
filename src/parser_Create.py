4dccbfad633c73a9b44cdbfef33440a0f3d654a9
drenight <lb1314@georgetown.edu>
2023-04-13 01:50:57 -0400

    CREATE base succeed

--- src/parser_Create.py
+++ src/parser_Create.py
@@ -1,14 +1,14 @@
 from antlr4 import *
-from antlr.SQLiteLexer import SQLiteLexer
-from antlr.SQLiteParser import SQLiteParser
-from antlr.SQLiteParserListener import SQLiteParserListener
+from parser.antlr.SQLiteLexer import SQLiteLexer
+from parser.antlr.SQLiteParser import SQLiteParser
+from parser.antlr.SQLiteParserListener import SQLiteParserListener
 
 class CreatePlan:
     def __init__(self):
         self.table_name = None
         self.columns = []  # 列的信息，每一项包含列名、数据类型、是否为主键等
         self.primary_key = None  # 主键的信息，包含主键名和包含哪些列
-        self.foreign_key = None 
+        self.foreign_key = {}
 
 class CreateListener(SQLiteParserListener):
     def __init__(self):
@@ -22,7 +22,24 @@ class CreateListener(SQLiteParserListener):
         if ctx.PRIMARY_():
             print(1)
         if ctx.FOREIGN_():
-            print(2)
+            constraint = ctx.foreign_key_clause()
+            foreign_key = {"table": constraint.foreign_table().getText()}
+            print(foreign_key)
+            local_columns = []
+            foreign_columns = []
+            print(ctx.indexed_column())
+
+            cnt = 0
+            for indexed_column in ctx.column_name():
+                print(indexed_column.getText())
+                local_columns.append(indexed_column.getText())
+                foreign_columns.append(ctx.foreign_key_clause().column_name()[0].getText())
+                cnt += 1
+
+            foreign_key["local_columns"] = local_columns
+            foreign_key["foreign_columns"] = foreign_columns
+            #constraints.append({"type": "FOREIGN KEY", "foreign_key": foreign_key})
+            self.plan.foreign_key = foreign_key
         return super().enterTable_constraint(ctx)
 
     def enterColumn_def(self, ctx:SQLiteParser.Column_defContext):
@@ -39,17 +56,7 @@ class CreateListener(SQLiteParserListener):
 
                 if constraint.PRIMARY_():
                     constraints.append({"type": "PRIMARYKEY"})
-                elif constraint.foreign_key_clause():
-                    #print(111)
-                    foreign_key = {"table": constraint.foreign_table().getText()}
-                    local_columns = []
-                    foreign_columns = []
-                    for indexed_column in constraint.indexed_column():
-                        local_columns.append(indexed_column.column_name().getText())
-                        foreign_columns.append(indexed_column.foreign_key_clause().column_name().getText())
-                    foreign_key["local_columns"] = local_columns
-                    foreign_key["foreign_columns"] = foreign_columns
-                    constraints.append({"type": "FOREIGN KEY", "foreign_key": foreign_key})
+                    self.plan.primary_key = column_name
 
         self.plan.columns.append({"name": column_name, "type": data_type, "constraints": constraints})
 
@@ -65,6 +72,31 @@ class MyListener(SQLiteParserListener):
     def enterTable_or_subquery(self, ctx:SQLiteParser.Table_or_subqueryContext):
         print("enter table_or_subquery:", ctx.getText())
 
+def virtual_plan_create(sql):
+    print(sql)
+    # 创建一个输入流
+    input_stream = InputStream(sql)
+
+    # 创建一个词法分析器
+    lexer = SQLiteLexer(input_stream)
+
+    # 创建一个词法记号流
+    token_stream = CommonTokenStream(lexer)
+
+    # 创建一个语法分析器
+    parser = SQLiteParser(token_stream)
+
+    # 解析 SQL 语句，生成语法树
+    tree = parser.parse()
+
+    # 创建一个 Listener 实例
+    listener = CreateListener()
+
+    # 遍历语法树
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    return listener.plan
+
 def main():
     # 要解析的 SQL 语句
     sql = """
@@ -75,6 +107,15 @@ def main():
         FOREIGN KEY (customer_id) REFERENCES customers(id)
         );
     """
+    ## {
+    # 'table_name': 'orders', 
+    # 'columns': [
+    #   {'name': 'id', 'type': 'INT', 'constraints': [{'type': 'PRIMARYKEY'}]}, 
+    #   {'name': 'customer_id', 'type': 'INT', 'constraints': []}, 
+    #   {'name': 'order_date', 'type': 'DATE', 'constraints': []}], 
+    # 'primary_key': 'id', 
+    # 'foreign_key': {'table': 'customers', 'local_columns': ['customer_id'], 'foreign_columns': ['id']}
+    # }
 
     # 创建一个输入流
     input_stream = InputStream(sql)
