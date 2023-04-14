a67227966f8531d6ad72328a8c5d7c5618f92b85
drenight <lb1314@georgetown.edu>
2023-04-14 10:33:19 -0400

    template ready

--- src/parser_TEMPLATE.py
+++ src/parser_TEMPLATE.py
@@ -1,19 +1,25 @@
+# 复制
 from antlr4 import *
 from parser.antlr.SQLiteLexer import SQLiteLexer
 from parser.antlr.SQLiteParser import SQLiteParser
 from parser.antlr.SQLiteParserListener import SQLiteParserListener
 
+# 核心要改的内容
 class CreatePlan:
     def __init__(self):
+        # 这些属性，要改，根据你是什么语句，决定你要存哪些参数
         self.table_name = None
         self.columns = []  # 列的信息，每一项包含列名、数据类型、是否为主键等
         self.primary_key = None  # 主键的信息，包含主键名和包含哪些列
         self.foreign_key = {}
 
+# 核心要改的内容
 class CreateListener(SQLiteParserListener):
     def __init__(self):
         self.plan = CreatePlan()
 
+    # 开发的时候下面删掉
+    # enter写那些这条语句，的语法树的各个节点的enter，拿信息
     def enterCreate_table_stmt(self, ctx:SQLiteParser.Create_table_stmtContext):
         # 获取表名
         self.plan.table_name = ctx.table_name().getText()
@@ -60,6 +66,7 @@ class CreateListener(SQLiteParserListener):
 
         self.plan.columns.append({"name": column_name, "type": data_type, "constraints": constraints})
 
+# 直接复制
 def virtual_plan_create(sql):
     print(sql)
     # 创建一个输入流
@@ -85,6 +92,7 @@ def virtual_plan_create(sql):
     walker.walk(listener, tree)
     return listener.plan
 
+# 自测这个单个模块用的
 # CREATE TABLE TTT(ID INT PRIMARY KEY);
 def main():
     # 要解析的 SQL 语句
